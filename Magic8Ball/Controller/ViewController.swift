07f0379d4f3c2e47d0a40cc2331111c000694feb
uszynskimateusz <uszynskimateusz@gmail.com>
2020-08-27 17:34:16 +0200

    randomise ball added

--- Magic8Ball/Controller/ViewController.swift
+++ Magic8Ball/Controller/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    let imageArray = [#imageLiteral(resourceName: "askagainlater"), #imageLiteral(resourceName: "theanswerisyes"), #imageLiteral(resourceName: "ihavenoidea"), #imageLiteral(resourceName: "yes"), #imageLiteral(resourceName: "no")]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        imageView.image = imageArray[Int.random(in: 0..<imageArray.count)]
     }
     
 }
-
